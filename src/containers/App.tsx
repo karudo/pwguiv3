da1db9cf7586fe7812571cc54738448c8d30d057
Deonis Peretyagin <karudo@pushwoosh.com>
2016-10-11 18:17:02 +0700

    tests

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,14 +1,11 @@
-import * as React from 'react'
+import * as React from 'react';
 import Qwe from './qwe';
 //import {createStore, applyMiddleware, combineReducers} from 'redux';
 
-interface Counter {
-  (start: number): string;
-  interval: number;
-  reset(): void;
-}
+import './test';
 
-class App extends React.Component<null, {num: number}> {
+
+class App extends React.Component<{}, {num: number}> {
   constructor(...args: any[]) {
     super(...args);
     this.state = {
@@ -20,7 +17,6 @@ class App extends React.Component<null, {num: number}> {
 
     return (
       <div>
-
         <button onClick={() => this.setState({num: num + 1})}>ok</button>
         hello!!!!
         <Qwe num={num} />
---
+++ src/containers/test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+class Asd<T> extends React.Component<{qwe: any}, {}> {
+  render() {
+    return <div>{this.props.qwe}</div>;
+  }
+}
+
+
+export default Asd
